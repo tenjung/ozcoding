c48812dfd6f2161968abf0eefed64c76e160a734
tenjung <doriclan@naver.com>
2025-11-12 15:15:25 +0900

    Delete notefolio directory

--- notefolio/src/components/common/index.js
+++
@@ -1,5 +0,0 @@
-export { AppHeader } from "./AppHeader";
-export { AppFooter } from "./AppFooter";
-export { AppStickyMenu } from "./AppStickyMenu";
-export { AppPortfolio } from "./AppPortfolio";
-export { ImageDialogCard } from "./ImageDialogCard";
